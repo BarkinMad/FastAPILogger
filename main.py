4484cdd9ef3371545505da3e8a0d08b6b48c285d
BarkinMAD <soulz.b.mad@gmail.com>
2022-12-08 11:08:09 -0500

    ff

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ app = FastAPI()
 async def log_error(request: Request):
     # Parse the JSON-encoded error message from the request
     requestInfo = await request.json()
-    print(type(requestInfo))
-    print(requestInfo)
-    #error_message = requestInfo["error"]
+    error_message = requestInfo["error"]
 
     # Open the CSV file in append mode
-    #with open("errors.csv", "a") as csvfile:
-        #writer = csv.writer(csvfile)
+    with open("errors.csv", "a") as csvfile:
+        writer = csv.writer(csvfile)
         # Write the error message to the CSV file
-        #writer.writerow([error_message])
-    #logger.info("Wrote new error to csv")
+        writer.writerow([error_message])
     # Return a success response
     return {"status": "SUCCESS"}
+    
