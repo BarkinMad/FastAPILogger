9fcf56c804ce96f5a8912fb99345017e4c0f54b7
BarkinMAD <soulz.b.mad@gmail.com>
2022-12-08 09:41:17 -0500

    logger

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, logger
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import csv
@@ -10,4 +10,5 @@ async def log_error(error: str):
     with open("errors.csv", "w") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([error])
+    logger.info("Logged new error to csv.")
     return FileResponse("errors.csv")
