a298b94511f7bbd2b9b32e0d52749988185bd695
BarkinMAD <soulz.b.mad@gmail.com>
2022-12-08 09:37:30 -0500

    init

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
+import csv
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.post("/log-error")
+async def log_error(error: str):
+    with open("errors.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([error])
+    return FileResponse("errors.csv")
