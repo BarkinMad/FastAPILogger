cf5eeb379b933f87633c2217e03b8898b934a2f0
BarkinMAD <soulz.b.mad@gmail.com>
2022-12-08 11:40:06 -0500

    Streaming

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
-from fastapi import FastAPI, Request, Response, logger, FileResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
 import pandas as pd
+from io import BytesIO
 
 app = FastAPI()
 
@@ -9,7 +11,8 @@ async def log_error(request: Request):
     # Parse the JSON-encoded error message from the request
     requestInfo = await request.json()
     error_message = requestInfo["error"]
-
+    print("Error message received from game server")
+    print(error_message)
     # Open the CSV file in append mode
     with open("errors.csv", "a") as csvfile:
         writer = csv.writer(csvfile)
@@ -20,5 +23,11 @@ async def log_error(request: Request):
 
 @app.get("/csv")
 async def get_csv():
-    df = pd.read_csv("./errors.csv")
-    return FileResponse(df.to_csv, media_type="text/csv")
+    df = pd.read_csv("errors.csv")
+
+    async def stream():
+        yield df.to_csv(index=False)
+    
+    response = StreamingResponse(stream(), media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=data.csv"
+    return response
